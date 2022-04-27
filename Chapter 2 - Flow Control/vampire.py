9a3fc6c365cc35b7a0f08ff7653270fdfb06a2b9
Neil Martin <87085416+NeilMartinDev@users.noreply.github.com>
2022-04-27 11:59:22 +0100

    playing about with previous exercise

--- Chapter 2 - Flow Control/vampire.py
+++ Chapter 2 - Flow Control/vampire.py
@@ -1,8 +1,15 @@
-if name == 'Alice':
-    print('Hi, Alice.')
-elif age < 12:
-    print('You are not Alice, kiddo.')
-elif age > 100:
-    print('You are not Alice, grannie.')
-elif age > 2000:
-      print('Unlike you, Alice is not an undead, immortal vampire.')
+while True:
+    print('Who are you?')
+    name = input()
+    print('What is your age?')
+    age = int(input())
+    if name == 'Alice' and age == 24:
+        print('Hi, Alice.')
+    elif age <= 12:
+        print('You are not Alice, kiddo.')
+    elif age > 100 and age < 1999:
+        print('You are not Alice, grannie.')
+    elif age > 2000:
+        print('Unlike you, Alice is not an undead, immortal vampire.')
+
+    break
