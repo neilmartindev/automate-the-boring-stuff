f3ebbd36c989b3d9c622204341aadddaf81a2349
Neil Martin <87085416+NeilMartinDev@users.noreply.github.com>
2022-04-27 12:17:09 +0100

    Cats cats cats

---
+++ Chapter 4 - Lists/Chapter4.py
@@ -0,0 +1,17 @@
+import sys
+
+spam = ["cat", "bat", "rat", "elephant"]
+
+print(spam[0])
+
+print(spam[1])
+
+print(spam[2])
+
+print(spam[3])
+
+print ('Hello ' + spam[0])
+
+print('The '+ spam[1] + ' ate the ' + spam[0] + '.')
+
+print(len(spam))
