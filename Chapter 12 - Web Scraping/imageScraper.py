ad4b5ded18580edc0629c2ac798845573d965399
Neil Martin <87085416+NeilMartinDev@users.noreply.github.com>
2022-05-11 09:43:30 +0100

    chapter 13

---
+++ Chapter 12 - Web Scraping/imageScraper.py
@@ -0,0 +1,23 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+site = 'http://bbc.co.uk'
+
+response = requests.get(site)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+img_tags = soup.find_all('img')
+
+urls = [img['src'] for img in img_tags]
+
+for url in urls:
+    filename = re.search(r'/([\w_-]+[.](jpg|gif|png))$', url)
+    if not filename:
+        print('Regex did not match with the URL: {}'.format(url))
+        continue
+    with open(filename.group(1), 'wb') as f:
+        if 'http' not in url:
+            url = '{}{}'.format(site, url)
+        response = requests.get(url)
+        f.write(response.content)
